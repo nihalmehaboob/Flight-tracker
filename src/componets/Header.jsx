a42359bdb8a245e3238f9c00f4c00f2d2aa71271
Mohamed NIHAL K <115793246+nihalmehaboob@users.noreply.github.com>
2024-07-09 01:58:03 +0530

    flight symbol is added

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,8 +1,17 @@
 import '../styles/header.css';
+import styled from 'styled-components';
+import flightImage from '../assets/flight.png';
+
+const AvatarImage = styled.img`
+  width: 30px; /* Adjust size as needed */
+  height: 30px;
+  border-radius: 50%; /* Makes the image circular */
+`;
 
 const Header = () => {
   return (
     <header className="header">
+    <AvatarImage  src={flightImage} alt="Flight Icon"/>
       <h3 className="title">FLY WORLD</h3>
       <nav>
         <ul className="nav-list">
