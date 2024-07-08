543c375f2b2e0698e46099726fb56ff7e5471c03
Mohamed NIHAL K <115793246+nihalmehaboob@users.noreply.github.com>
2024-07-09 02:19:10 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Headers from './componets/Header';
+import Main from './componets/Main';
 function App() {
  
 
   return (
     <>
     <Headers/>
+    <Main/>
     </>
   )
 }
---
+++ src/componets/Main.jsx
@@ -0,0 +1,11 @@
+const Main=()=>{
+    return(
+        <>
+       <div className="main_dev">
+       <h1 >Fly WORLD</h1>
+       </div>
+        </>
+    );
+};
+
+export default Main ;
