ff0eeb2a2d42c809cacb6657c5010d138985ed96
Mohamed NIHAL K <115793246+nihalmehaboob@users.noreply.github.com>
2024-07-09 12:46:38 +0530

    useState hook added

--- src/componets/Main.jsx
+++ src/componets/Main.jsx
@@ -1,5 +1,27 @@
 import '../styles/mian.css'
+import { useState } from "react";
+
+
 const Main=()=>{
+const [origin,setOrigin]=useState("");
+const [destination, setDestination]=useState("");
+
+const handleOriginChange = (e) => {
+    setOrigin(e.target.value);
+    console.log(origin);
+  };
+
+  const handleDestinationChange = (e) => {
+    setDestination(e.target.value);
+    console.log(destination);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission, e.g., send the data to the backend or display it
+    console.log('Origin:', origin);
+    console.log('Destination:', destination);
+  };
+
     return(
         <>
        <div className="main_dev">
@@ -7,11 +29,11 @@ const Main=()=>{
        <p className='text-sm-start'>When once you have tasted flight, you will forever walk the earth with your eyes turned skyward,</p>
        <p className='text-sm-start'> for there you have been, and there you will always long to return.</p>
        <div className='box1'>
-        <form >
+        <form onSubmit={handleSubmit}>
         <label className='form-label'>Enter your origin:
-        <input type="text" />
+        <input type="text" value={origin} onChange={handleOriginChange}/>
       </label>
-        <label className='form-label'>Enter your Destination:
+        <label className='form-label' value={destination} onChange={handleDestinationChange}>Enter your Destination:
         <input type="text" />
       </label>
       <button type="submit" className="btn btn-primary">Submit</button>
